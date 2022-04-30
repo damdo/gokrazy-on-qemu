01ca7673d4506ea7dfdb63f8b7ce204df0ab9231
Damiano Donati <damiano.donati@gmail.com>
2022-04-30 23:04:44 +0100

    use MACHINE env var

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-arch="${GOARCH:=amd64}"
+arch="${MACHINE:=amd64}"
 case $arch in
   arm64)
     qemu-system-aarch64 \
@@ -45,15 +45,15 @@ case $arch in
   amd64)
     qemu-system-x86_64 \
       -name gokrazy-amd64 \
-      -m 4096 \
+      -m 4G \
       -smp $(nproc) \
       -usb \
       -nographic \
       -serial mon:stdio \
       -boot order=d \
       -drive file=drive.img,format=raw \
-      -netdev user,id=mynet0,hostfwd=tcp::8080-:80,hostfwd=tcp::2222-:22 \
-      -device e1000,netdev=mynet0
+      -netdev user,id=net0,hostfwd=tcp::8080-:80,hostfwd=tcp::2222-:22 \
+      -device e1000,netdev=net0
     ;;
   *)
     echo -n "unsupported arch ${arch}"
