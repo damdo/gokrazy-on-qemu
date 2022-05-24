4a1043ca7bad6c858c77000355faf25ac8d959bc
Damiano Donati <damiano.donati@gmail.com>
2022-05-24 22:25:27 +0200

    build.sh: add single img and multi(file) zip modes

--- build.sh
+++ build.sh
@@ -3,6 +3,12 @@
 # ---------------------------
 # VARS
 # ---------------------------
+
+# output chooses whether to output a single drive img or multiple partition imgs
+# single: one drive.img
+# multi: boot: boot.img, root: root.squashfs, mbr: mbr.img
+# default is: single
+output="${OUTPUT:=single}"
 gokr_packer_version="v0.0.0-20220507152425-0d3aef012e03"
 gokr_packer_base="github.com/gokrazy/tools/cmd/gokr-packer"
 hostname="gokrazy"
@@ -72,6 +78,9 @@ done
 # if a qemu machine with `-name gokrazy` is already running, then update and write to file
 # else just write to file.
 if [[ "$(ps -ef | grep qemu-system- | grep gokrazy | wc -l)" -eq 1 ]]; then
+  if [[ "$output" == "multi" ]]; then
+    echo "ERROR: would update OTA the existing gokrazy installation but can't because 'multi' mode is set, should be 'single'."
+  fi
   shouldupdate="http://gokrazy:$(cat ~/.config/gokrazy/http-password.txt)@127.0.0.1:8080/"
 else
   shouldupdate=""
@@ -80,19 +89,61 @@ fi
 # ---------------------------
 # GOKR-PACKER RUN
 # ---------------------------
+echo "building grokrazy img(s) in '$output' mode"
 
-GOOS=linux GOARCH="${arch}" GOBIN=$(pwd) ./gokr-packer \
- -hostname="${hostname}" \
- -kernel_package="${kernel_package}" \
- -firmware_package="${firmware_package}" \
- -target_storage_bytes=2147483648 \
- -overwrite=drive.img \
- -update="${shouldupdate}" \
- -serial_console="${serial_console}" \
+# arguments list
+args=()
+
+# Build up conditional arguments
+if [[ "$output" == "single" ]]; then
+  args+=(\
+   -overwrite=drive.img
+  )
+elif  [[ "$output" == "multi" ]]; then
+  args+=(\
+   -overwrite_boot=boot.img
+   -overwrite_root=root.squashfs
+   -overwrite_mbr=mbr.img
+  )
+fi
+
+args+=(
+ -hostname="${hostname}"
+ -kernel_package="${kernel_package}"
+ -firmware_package="${firmware_package}"
+ -target_storage_bytes=2147483648
+ -serial_console="${serial_console}"
+ -update="${shouldupdate}"
  ${unversioned_components[@]}
+)
+
+GOOS=linux GOARCH="${arch}" GOBIN=$(pwd) ./gokr-packer "${args[@]}"
+
+# ---------------------------
+# COMPRESS
+# ---------------------------
+if [[ "$output" == "multi" ]]; then
+  zip -r drive.zip mbr.img boot.img root.squashfs
+fi
 
 # ---------------------------
 # CLEANUP
 # ---------------------------
 # cleanup generated gokrazy artifacts
 rm -rf ./extrafiles ./flags ./env ./buildflags
+if [[ "$output" == "multi" ]]; then
+  rm boot.img root.squashfs mbr.img
+fi
+
+# ---------------------------
+# OUTPUT
+# ---------------------------
+echo ""
+echo "======"
+echo "OUTPUT"
+echo "======"
+if [[ "$output" == "single" ]]; then
+  echo "'$output' mode: drive.img has been generated."
+elif  [[ "$output" == "multi" ]]; then
+  echo "'$output' mode: drive.zip has been generated."
+fi
